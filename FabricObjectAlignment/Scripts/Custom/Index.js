8a474033e504da03fe926b50a0768c5a80121cfc
Jerrod <kh_duster@charter.net>
2018-05-25 22:36:00 -0500

    Added solution Fixed context issues This is not stable or testable. Progresss

---
+++ FabricObjectAlignment/Scripts/Custom/Index.js
@@ -0,0 +1,41 @@
+﻿Idx = {
+    jversion: null,
+    canvas: null,    
+    rectSettings: {
+        width: 100,
+        height: 100,
+        color: null
+    },
+    init: function () { 
+        Idx.jversion = jQuery.fn.jquery;
+        $('#version').append(Idx.jversion);
+        Idx.canvas = new fabric.Canvas('fabCanvas', { height: 500, width: 900 });
+        //I have found having 5 object is good for testing
+        Idx.canvas
+            //top-left
+            .add(new fabric.Rect(Idx.rectSettings).set({ 'fill': '#000' }))
+            //bottom-left
+            .add(new fabric.Rect(Idx.rectSettings).set({ 'fill': '#fc003c', 'top': Idx.canvas.get('height') - Idx.rectSettings.height }))
+            //bottom-right
+            .add(new fabric.Rect(Idx.rectSettings).set({
+                'fill': '#620088',
+                'top': Idx.canvas.get('height') - Idx.rectSettings.height,
+                'left': Idx.canvas.get('width') - Idx.rectSettings.width
+            }))
+            //top-right
+            .add(new fabric.Rect(Idx.rectSettings).set({ 'fill': '#2d18c7', 'left': Idx.canvas.get('width') - Idx.rectSettings.width }));
+        //I want to center the last one.
+        var r = new fabric.Rect(Idx.rectSettings).set('fill', '#008821');
+        Idx.canvas.add(r);
+        r.center();
+
+        Idx.events.init();
+    },
+    events: {
+        init: function () {
+            //Idx.canvas.
+        }
+    }
+};
+
+$(Idx.init)
---
+++ FabricObjectAlignment/Scripts/Custom/__references.js
@@ -0,0 +1,2 @@
+﻿/// <reference path="../jquery-3.3.1.js" />
+/// <reference path="alignobjects.js" />
